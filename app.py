5ec960e844f2dc23070980af75d14461875f3728
pradheepa73 <pradheepa378@gmail.com>
2026-06-06 16:20:54 +0530

    deployment update

--- app.py
+++ app.py
@@ -34,5 +34,8 @@ def analyze():
         "crack_time": crack_time
     })
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
